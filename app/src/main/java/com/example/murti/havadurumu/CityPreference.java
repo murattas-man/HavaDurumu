ac9071a74b0faddb797568eb053e8ce3d6b398fe
murat tas <murattas721@gmail.com>
2018-03-21 22:04:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/murti/havadurumu/CityPreference.java
@@ -0,0 +1,27 @@
+package com.example.murti.havadurumu;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+/**
+ * Created by murti on 03.03.2018.
+ */
+
+public class CityPreference {
+
+    SharedPreferences prefs;
+
+    public CityPreference(Activity activity){
+        prefs = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+
+    // If the user has not chosen a city yet, return
+    // Sydney as the default city
+    String getCity(){
+        return prefs.getString("city", "istanbul, TR");
+    }
+
+    void setCity(String city){
+        prefs.edit().putString("city", city).commit();
+    }
+}
---
+++ app/src/main/java/com/example/murti/havadurumu/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.murti.havadurumu;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new WeatherFragment())
+                    .commit();
+        }
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.weather, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.change_city){
+            showInputDialog();
+        }
+        return false;
+    }
+
+    private void showInputDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Şehir Seç");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        builder.setPositiveButton("Bul", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                changeCity(input.getText().toString());
+            }
+        });
+        builder.show();
+    }
+
+    public void changeCity(String city){
+        WeatherFragment wf = (WeatherFragment)getFragmentManager()
+                .findFragmentById(R.id.container);
+        wf.changeCity(city);
+        new CityPreference(this).setCity(city);
+    }
+}
